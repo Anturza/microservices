1b787025170a96ff6fb01ea7d56cd0288b0816ce
Anturza <anturza@gmail.com>
2026-02-20 13:05:25 +0400

    Added jpaapi module

---
+++ jpaapi/src/main/java/mod4/jpaapi/messaging/UserEventProducer.java
@@ -0,0 +1,34 @@
+package mod4.jpaapi.messaging;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class UserEventProducer {
+
+    @Value("${app.topic-name}")
+    private String topicName;
+
+    private final KafkaTemplate<String, UserEvent> kafkaTemplate;
+
+    public UserEventProducer(KafkaTemplate<String, UserEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendUserEvent(UserEvent event) {
+        CompletableFuture<SendResult<String, UserEvent>> future = kafkaTemplate.send(topicName, event);
+
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                System.out.println("Sent message=[" + event +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            } else {
+                System.err.println("Unable to send message=[" + event + "] due to : " + ex.getMessage());
+            }
+        });
+    }
+}
