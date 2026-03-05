1aa6f4471439aed118dd2b7b9d3b9977b990f7b7
Anturza <anturza@gmail.com>
2026-03-05 13:15:42 +0400

    Added and configured ConfigServer Moved mailapp properties to remote location (git) confugured mailapp local properties

--- mailapp/src/main/java/study/mailapp/controllers/TestController.java
+++ mailapp/src/main/java/study/mailapp/controllers/TestController.java
@@ -1,5 +1,6 @@
 package study.mailapp.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
     @GetMapping
     public String test() {
-        return "it's ok";
+        return String.format("It's OK. app instance is: %s", instanceId);
     }
 }
